f90a232c2e7b12e3812c55fe28393e88d17e673e
Matteo Sala <salazar34@outlook.it>
2022-01-14 15:30:10 +0100

    Finished Navbar. Started working on City security page.
    Implementing Vue multi-pages system.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Navbar from './components/Navbar.vue'
+import MainContent from "./components/MainContent.vue"
 import "./tailwind.css"
 </script>
 
 <template>
   <Navbar />
+  <MainContent />
 </template>
 
 
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,10 +5,11 @@
   <nav class="shadow-2xl">
     <div class="max-6xl mx-auto px-4 shadow-2xl">
       <div class="flex justify-between">
-        <div class="flex space-x-7">
+        <div class="flex">
           <a href="#" class="navbar-brand flex items-center py-6 px-10 text-3xl uppercase transition ease-in-out">
             Sus-Landia
           </a>
+          <img class="py-4 logo" src="../assets/images/psyduck-logo.png" alt="" width="50">
         </div>
         <div class="flex px-20 gap-10">
           <a href="#" class="flex items-center text-lg transition ease-in-out hover:text-indigo-400 duration-500">Home</a>
@@ -25,4 +26,5 @@
   </nav>
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
