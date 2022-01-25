58139d644b84fd6628842b3235c277bb95bba6f4
Matteo Sala <salazar34@outlook.it>
2022-01-25 19:50:38 +0100

    Continuing working on new home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,39 @@
 <template>
   <Navbar />
   <div class="container mx-auto py-10">
-    <div class="flex flex-col items-center justify-center">
-      <h1 class="text-4xl">BASSO - The future for all cities</h1>
-      <img
-        class="mt-10"
-        src="../assets/Temp.png"
-        alt="BASSO Logo"
-        width="250"
-      />
-      <div class="grid grid-cols-3 gap-5">
-        <!-- <Column v-for="column in columns" v-bind:key="column.id" /> -->
+    <div
+      class="max-w-6xl gap-36 flex flex-row items-center justify-between mx-auto py-20"
+    >
+      <img src="../assets/Temp.png" alt="" width="300" />
+      <div class="flex flex-col gap-5">
+        <h1 class="lg:text-6xl">BASSO - A new way to live the city</h1>
+        <p class="lg:text-lg">
+          The main place in which most industrial activities take place is the
+          city which, to date, represents a serious problem for pollution that
+          must be solved with timely interventions.
+        </p>
       </div>
+    </div>
+    <div
+      class="custom-container-mw flex flex-row items-center justify-between mt-20 mx-auto"
+    >
+      <div class="flex flex-col">
+        <h1 class="lg:text-5xl uppercase text-center mb-10">The problem</h1>
+        <p class="custom-mw">
+          In the last decades the percentage of pollution in all cities and
+          metropolis is constantly increasing, due to the continuous use of
+          polluting vehicles and unsustainable energies. This big problem, thay
+          has been undervalued for years, is now surfacing with all the releated
+          consequences, i.e.
+          <ul class="list-decimal custom-mw mt-5">
+            <li>Increased mortality related to cardivascular and respiratory diseases</li>
+            <li>Rising global temperatures resulting in melting glaciers and increased natural disasters.</li>
+            <li>Continued extinction of animal species, caused by climate anomalies and/or continued predator migrations.</li>
+            <li>Disruption of natural ecosystems and destruction of animal habitats.</li>
+          </ul>
+        </p>
+      </div>
+      <img src="../assets/pollution.png" alt="Polluted city" width="500" />
     </div>
   </div>
   <Footer />
@@ -20,26 +42,23 @@
 <script>
 import Navbar from "../components/Navbar.vue";
 import Footer from "../components/Footer.vue";
-import Column from "../components/Column.vue";
 
 export default {
   name: "Home",
   components: {
     Navbar,
     Footer,
-    Column,
-  },
-  data: function () {
-    return {
-      columns: [
-        {
-          id: 1,
-          header: "",
-        },
-      ],
-    };
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.custom-mw{
+  @apply lg:text-lg;
+  max-width: 650px;
+}
+
+.custom-container-mw{
+  max-width: 90rem;
+}
+</style>
