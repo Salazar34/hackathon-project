f3141e358696b2409bd10bcec4a5f4a5b9b23165
Matteo Sala <salazar34@outlook.it>
2022-01-27 19:55:53 +0100

    Finished home page with responsive layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,15 @@
 <template>
   <Navbar />
-  <div class="container mx-auto py-10">
+  <div class="container mx-auto lg:py-10">
     <div
-      class="max-w-5xl gap-36 flex flex-row items-center justify-between mx-auto py-20"
+      class="max-w-5xl lg:gap-36 gap-20 flex lg:flex-row flex-col items-center justify-between mx-auto py-20"
     >
       <img src="../assets/Temp.png" alt="" width="300" />
       <div class="flex flex-col gap-7">
-        <h1 class="lg:text-6xl">BASSO - A new way to live the city</h1>
-        <p class="lg:text-lg">
+        <h1 class="lg:text-6xl text-3xl lg:text-left text-center">
+          BASSO - A new way to live the city
+        </h1>
+        <p class="lg:text-lg lg:text-left text-center">
           The main place in which most industrial activities take place is the
           city which, to date, represents a serious problem for pollution that
           must be solved with timely interventions.
@@ -15,10 +17,12 @@
       </div>
     </div>
     <div
-      class="custom-container-mw flex flex-row items-center justify-between mt-40 mx-auto"
+      class="custom-container-mw flex lg:flex-row flex-col items-center justify-between lg:mt-40 mt-20 mx-auto"
     >
-      <div class="flex flex-col">
-        <h1 class="lg:text-5xl uppercase text-center mb-10">The problem</h1>
+      <div class="flex flex-col text-justify lg:w-full w-5/6">
+        <h1 class="lg:text-5xl text-3xl uppercase text-center mb-10">
+          The problem
+        </h1>
         <p class="custom-mw">
           In the last decades the percentage of pollution in all cities and
           metropolis is constantly increasing, due to the continuous use of
@@ -44,12 +48,17 @@
           </li>
         </ul>
       </div>
-      <img src="../assets/pollution.png" alt="Polluted city" width="500" />
+      <img
+        class="w-4/5 mt-10"
+        src="../assets/pollution.png"
+        alt="Polluted city"
+        width="500"
+      />
     </div>
-    <h1 class="lg:text-4xl text-center uppercase mt-40">
+    <h1 class="lg:text-4xl text-3xl text-center uppercase lg:mt-40 mt-36">
       Pollution consequences
     </h1>
-    <div class="flex flex-row items-center justify-center mx-auto">
+    <div class="container mx-auto grid lg:grid-cols-2 place-items-center">
       <div class="grid lg:grid-cols-2 mt-20">
         <div
           v-for="column in columns"
@@ -64,13 +73,18 @@
         </div>
       </div>
       <div class="container flex flex-col items-center justify-center">
-        <p class="lg:text-lg lg:w-1/2">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste
-          cupiditate iusto, neque laborum fugiat vitae repellendus architecto
-          animi officia debitis commodi labore, aperiam, autem laudantium
-          quisquam omnis sed nisi nulla.
+        <p
+          class="lg:text-lg lg:w-4/5 leading-relaxed text-justify lg:w-full w-5/6"
+        >
+          Cities, as primary places in the conduct of daily activities and with
+          the highest concentration of movement, is the cause of most
+          environmental pollution. The high concentration of polluting
+          substances in the city allows a simple diffusion in every part of the
+          atmosphere, thanks also to the intervention of the air currents. This
+          leads to the spread of these substances throughout the world, causing
+          serious problems:
         </p>
-        <ul class="list-decimal lg:text-lg lg:w-1/2 mt-10">
+        <ul class="list-decimal lg:text-lg lg:w-4/5 w-5/6 text-justify mt-10">
           <li class="mb-5">
             Increased mortality related to cardivascular and respiratory
             diseases
@@ -88,6 +102,39 @@
           </li>
         </ul>
       </div>
+    </div>
+    <div
+      class="custom-container-mw flex lg:flex-row flex-col items-center justify-between mt-36 mx-auto"
+    >
+      <div class="flex flex-col lg:w-full text-justify w-5/6">
+        <h1 class="lg:text-5xl text-3xl uppercase text-center mb-10">
+          The solution
+        </h1>
+        <p class="custom-mw">
+          The weapon we need to use to fight the pollution and its releated
+          problems are sustainable energies and the common sense of allò the
+          citizens. The use of sustainable energies as primary energy sources
+          prevents our planet to be destroied. Most atmospheric pollution comes
+          from cities, more specifically
+        </p>
+        <ul class="list-decimal custom-mw mt-5">
+          <li>Pollution caused by smog from means of transport.</li>
+          <li>
+            Pollution caused by the high consumption of methane and gas (380
+            million tonnes of methane per year, ISTAT).
+          </li>
+          <li>Pollution from discharges of toxic waste by industry.</li>
+          <li>
+            Pollution caused by the release of waste into the environment.
+          </li>
+        </ul>
+      </div>
+      <img
+        class="lg:w-full w-5/6 mt-10"
+        src="../assets/futured-city.jpg"
+        alt="Futured city"
+        width="500"
+      />
     </div>
   </div>
   <Footer />
@@ -151,6 +198,10 @@ export default {
 .custom-mw {
   @apply lg:text-lg leading-relaxed;
   max-width: 550px;
+}
+
+.custom-mw li:not(:last-child) {
+  @apply mb-5;
 }
 
 .column {
